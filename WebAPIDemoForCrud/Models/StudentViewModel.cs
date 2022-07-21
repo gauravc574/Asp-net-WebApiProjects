e3c9880888875776a6962375138802c90aa4839d
gauravc574 <gauravchawre580@gmail.com>
2022-07-21 13:48:50 +0530

    Created web apis for CRUD

---
+++ WebAPIDemoForCrud/Models/StudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemoForCrud.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        public string StudentName { get; set; }
+        
+
+        public AddressViewModel Address { get; set; }
+        public StandardViewModel Standard { get; set; }
+    }
+}
